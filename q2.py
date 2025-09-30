0099bb710d278a31c1677428ffafd768717b43d9
unknown <wesleydasilvaalmeida706@gmail.com>
2025-09-30 12:28:22 -0300

    questao 2

--- q2.py
+++ q2.py
@@ -1,8 +1,8 @@
 #questão 7
+n = int(input("digite um numero inteiro: "))
 def par_impar(n):
     if n % 2 == 0:
         print("o numero é par")
     else:
         print("o numero é impar")
-par_impar(7)
-par_impar(2)
+par_impar(n)
